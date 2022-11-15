95f23b68aafe25c687db769a4c3dbc7d633f8e9d
TobiWan8 <tobiaslarsen93@gmail.com>
2022-11-15 12:56:31 +0100

    Login via user done. need fix enterlogin and login for admin

--- View/LoginView.xaml.cs
+++ View/LoginView.xaml.cs
@@ -11,13 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace Kørselslog.View
 {
-    /// <summary>
-    /// Interaction logic for LoginView.xaml
-    /// </summary>
     public partial class LoginView : Window
     {
         public LoginView()
@@ -43,7 +41,40 @@ namespace Kørselslog.View
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
         {
-            string connectionString = "@"Data Source = DESKTOP-RDJ3VF9;
+            string connectionString = @"Server = DESKTOP-RDJ3VF9; Initial Catalog = Korselslog; persist security info = true; User ID=sa; Password=funnyHAHA";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            try
+            {
+                if(sqlConnection.State == ConnectionState.Closed)
+                    sqlConnection.Open();
+
+                string query = "SELECT COUNT(1) FROM [User] WHERE Username=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlConnection);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUser.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPass.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                
+                if(count == 1)
+                {
+                    MainWindowView dashboard = new MainWindowView();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
         }
     }
 }
